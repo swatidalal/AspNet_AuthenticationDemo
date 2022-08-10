f2d666f8ef64382bec76185dfd9001bfc6766b8e
swatidalal <swati15091999@gmail.com>
2022-08-10 19:52:17 +0530

    Add project files.

---
+++ AspNet_IdentityDemo/Controllers/RoleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_IdentityDemo.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var role = _roleManager.Roles.ToList();
+            return View(role);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await _roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ AspNet_IdentityDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNet_IdentityDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
